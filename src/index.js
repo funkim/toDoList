9550deedf5d2ed5828eb6830635fb5155b86d1ff
funkim <mcking172@skiff.com>
2024-04-11 21:00:38 -0400

    made create new project button and added a button which deletes the project it's attached to if clicked.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import { getUserProjectInputs } from "./todo"; 
 import { createProject } from "./todo";
+import { createProjectListener } from "./todo";
 import { style } from "./style.css"
+const landingPage = document.querySelector(".landingPage")
+const projectForm = document.querySelector('.addNewProjectPage')
+
+
+createProjectListener();
 const projectSubmit = document.getElementById("projectCreator");
 projectSubmit.addEventListener('submit', function(e) {
     e.preventDefault();
     const { projectName, projectDueDate, projectDescription, priority } = getUserProjectInputs();
     createProject(projectName,  projectDueDate.value, projectDescription, priority );
+    projectForm.classList.add("hidden")
+    landingPage.classList.remove("hidden")
 });
--- src/todo.js
+++ src/todo.js
@@ -1,7 +1,5 @@
 
-
 export function getUserProjectInputs() {
-    const projectForm = document.querySelector('.addNewProjectPage form');
     const projectName = document.getElementById('projectName').value;
     const projectDueDate = document.getElementById('projectDueDate');
     const projectDescription = document.getElementById('projectDescription').value;
@@ -18,7 +16,8 @@ export function getUserProjectInputs() {
 }
 
 export function createProject(name, dueDate, description, priority) {
-    
+ 
+    const projectForm = document.querySelector('.addNewProjectPage');
     const allProjectsContainer = document.getElementById("projects");
     const projectContainer = document.createElement("div");
     const projectContainerHeader = document.createElement("h1");
@@ -36,10 +35,32 @@ projectContainerHeader.classList.add("projectContainerHeader")
 projectContainerPriority.classList.add("projectContainerPriority")
 projectContainerDueDate.classList.add("projectContainerDueDate")
 projectContainerDescription.classList.add("projectContainerDescription")
-
+deleteProject(projectContainer);
 
 projectContainerHeader.innerHTML = name
 projectContainerPriority.innerHTML = priority 
 projectContainerDueDate.innerHTML = dueDate
 projectContainerDescription.innerHTML= description
+projectForm.classList.add()
+}
+
+function deleteProject(projectContainer) {
+    const deleteProjectButton = document.createElement("input");0
+    deleteProjectButton.setAttribute("type", "button");
+    projectContainer.appendChild(deleteProjectButton);
+
+    deleteProjectButton.addEventListener('click', function() {
+    projectContainer.remove();
+    }
+)}
+
+export function createProjectListener() {
+const createProjectButton = document.querySelector('.createNewProject')
+const landingPage = document.querySelector(".landingPage")
+const projectForm = document.querySelector('.addNewProjectPage')
+
+createProjectButton.addEventListener('click', function() {
+    landingPage.classList.add("hidden")
+    projectForm.classList.remove("hidden")
+})
 }
